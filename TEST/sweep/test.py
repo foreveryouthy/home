ebda0147ac016ac58ef89c3c123021ff9d720ed6
zhoush103567 <zhoush103567@piceacorp.com>
2024-11-18 09:42:55 +0800

    add pymysql

--- TEST/sweep/test.py
+++ TEST/sweep/test.py
@@ -1,2 +1,17 @@
-print(hello)
-print(hello)
+import pymysql
+
+db = pymysql.connect(host='10.6.0.103',
+user='root',password='123456',database='sys')
+
+cursor = db.cursor()
+
+cursor.execute('SELECT VERSION()')
+
+
+DATA = cursor.fetchone()
+
+print(DATA)
+
+db.close()
+
+
